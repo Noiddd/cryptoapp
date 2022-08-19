86f6f340b929b16ae977a84e4e442668f9ff36d3
Noiddd <dionang11@gmail.com>
2022-08-19 17:41:44 +0800

    01:14:50

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { Select, Typography, Row, Colm, Avatar, Card } from "antd";
+import moment from "moment";
+
 const News = () => {
   return <div>News</div>;
 };
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "32f9a5d291msh2a5c5885c666384p108f6cjsnedabe94d5172",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
